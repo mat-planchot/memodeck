2dadadfe667f5b279a68f890cc55fc88197352fc
mat-planchot <mat_planchot@hotmail.fr>
2021-06-07 23:44:13 +0200

    update node.js.yml

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 // Init environment
 require('dotenv').config();
-const port = Number(process.env.PORT || 3331);
+const port = Number(process.env.PORT || 3000);
 // starting the server
 app.listen(port, () => console.log(`🚀 Server running on port ${port}!`));
