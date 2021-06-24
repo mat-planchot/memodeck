8cacee67c210b034e264f9fc9e00152e7b19a810
jawadjem <elmousaouijawad78@gmail.com>
2021-06-24 17:45:49 +0200

    commentaire anglais jawad

--- src/models/card.model.js
+++ src/models/card.model.js
@@ -2,9 +2,44 @@ const query = require('../db/db-connection');
 const { multipleColumnSet } = require('../utils/common.utils');
 const Role = require('../utils/userRoles.utils');
 
+
+
+/******************************************************************************
+ *                              Card Model
+ *
+ * An element that contains the data as well as logic related to the data: validation, reading and recording.
+ * It contain only a simple value, or a more complex data structure.
+ * This model represents the inserts of new cards, the update or deletion of cards
+
+
+ * Steps of all methods :
+ *  1. Adding new cards
+ *  2. Update cards
+ *  3. Removal of cards
+ *
+ *  Description of the columns in the database :
+ *
+ * idcard: number of card
+ * front: Questions
+ * back: Answers
+ * frontmedia: Questions with media
+ * backmedia: Answers with media
+ * fkdeck:
+ * nbreview:
+ * issuspended:
+ * difficulty:
+ * nbdayreview:
+ * reviewdate:
+ *
+ *
+ **/
+
+
 class CardModel {
-    tableName = 'card';
-    // idcard, front, back, frontmedia, backmedia, fkdeck, nbreview, issuspended, difficulty, nbdayreview, reviewdate
+    tableName = 'card'; /** TABLE NAME **/
+    /**
+     * get all the cards in the database with this data: idcard, front, back, frontmedia, backmedia, fkdeck, nbreview, issuspended, difficulty, nbdayreview, reviewdate
+     */
 
     find = async (params = {}) => {
         let sql = `SELECT * FROM ${this.tableName}`;
@@ -31,6 +66,19 @@ class CardModel {
         return result[0];
     }
 
+    /**
+     * Create a card with the body parameters
+     * @param front
+     * @param back
+     * @param frontmedia
+     * @param backmedia
+     * @param fkdeck
+     * @param nbreview
+     * @param issuspended
+     * @param difficulty
+     * @param nbdayreview
+     * @param reviewdate
+     */
     create = async ({ front, back, frontmedia=null, backmedia=null, fkdeck, nbreview=1, issuspended=0, difficulty=1, nbdayreview=1, reviewdate=null }) => {
         const sql = `INSERT INTO ${this.tableName}
         (front, back, frontmedia, backmedia, fkdeck, 
@@ -42,7 +90,11 @@ class CardModel {
 
         return affectedRows;
     }
-
+    /**
+     * Update a card from its URL id parameter and body parameters
+     * @param id
+     * @param params
+     */
     update = async (params, id) => {
         const { columnSet, values } = multipleColumnSet(params)
 
@@ -52,10 +104,14 @@ class CardModel {
 
         return result;
     }
-
+    /**
+     * Update the review date of card from its URL id parameter and body parameters
+     * @param id
+     * @param params
+     */
     updateReviewDate = async (params, id) => {
         const { columnSet, values } = multipleColumnSet(params)
-        
+
         const sql = `UPDATE card SET ${columnSet}, 
         reviewdate = (NOW() + INTERVAL ? DAY)
         WHERE idcard = ?`;
@@ -64,7 +120,10 @@ class CardModel {
 
         return result;
     }
-
+    /**
+     * Delete a card from its id
+     * @param id
+     */
     delete = async (id) => {
         const sql = `DELETE FROM ${this.tableName}
         WHERE idcard = ?`;
@@ -73,7 +132,10 @@ class CardModel {
 
         return affectedRows;
     }
-
+    /**
+     * get a random daily review card from a deck
+     * @param fkdeck id of a deck
+     */
     randomReviewCard = async (fkdeck) => {
         // idcard, front, back, frontmedia, backmedia, fkdeck, nbreview, issuspended, difficulty, nbdayreview, reviewdate
         let sql = `SELECT * FROM ${this.tableName}
@@ -85,7 +147,10 @@ class CardModel {
         // return back the first row (card)
         return result[0];
     }
-
+    /**
+     * get all daily review cards from a deck
+     * @param fkdeck id of a deck
+     */
     reviewCards = async (fkdeck) => {
         // idcard, front, back, frontmedia, backmedia, fkdeck, nbreview, issuspended, difficulty, nbdayreview, reviewdate
         let sql = `SELECT * FROM ${this.tableName}
